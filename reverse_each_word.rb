bd69dac9d921a8b65be47376e68827b1c55bb528
Andre Skeene <halodre24@gmail.com>
2020-02-17 20:09:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,14 +5,6 @@ def reverse_each_word(sentence)
   original_array.each do |word|
     return_array << word.reverse
   end
-  return_array.join(" ")
   
-  original_array = sentence.split
-  return_array = []
-  
-  original_array.collect do
-    return_array << sentence.reverse
-  end
-  return_array.join(" ")
 end
   
